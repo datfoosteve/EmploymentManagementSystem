113574fb4e7432cf6c9586fa57609539a6ed3663
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-02-20 19:43:29 -0600

    okay here we go

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,3 @@
---  `department`
-
---     * `id`: `INT PRIMARY KEY`
-
---     * `name`: `VARCHAR(30)` to hold department name
-
--- * `role`
-
---     * `id`: `INT PRIMARY KEY`
-
---     * `title`: `VARCHAR(30)` to hold role title
-
---     * `salary`: `DECIMAL` to hold role salary
-
---     * `department_id`: `INT` to hold reference to department role belongs to
-
--- * `employee`
-
---     * `id`: `INT PRIMARY KEY`
-
---     * `first_name`: `VARCHAR(30)` to hold employee first name
-
---     * `last_name`: `VARCHAR(30)` to hold employee last name
-
---     * `role_id`: `INT` to hold reference to employee role
-
---     * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
-
-
-
-
 DROP DATABASE IF EXISTS corpo_db;
 CREATE DATABASE corpo_db;
 
